c5e7c4f9a3ecb55f8c9f554891151c1185985b02
Lameck Matare <lameckfarai@gmail.com>
2017-03-16 16:03:56 +0200

    Created the app data

---
+++ data.js
@@ -0,0 +1,135 @@
+const menu = {
+  breakfast: [
+      {
+        title: "SOUTH AFRICAN BREAKFAST",
+        description: "2 eggs(done your way), 2 slices of toast(white, brown, rye, ciabata... your choice), 5 rashers bacon",
+        price: 108
+      },
+      {
+        title: "PASTRIES",
+        description: "Croissant, pain au chocolat, pain aux raisins or almond croissant",
+        price: 58
+      },
+      {
+        title: "FRENCH TOAST CROISSANT",
+        description: "Our twist on the classic - served warm and with walnut praline, crème fraîche and honey",
+        price: 58
+      },
+      {
+        title: "BACON & EGGS",
+        description: "",
+        price: 49
+      },
+  ],
+  dessert: [
+    {
+      title: "CRÈME BRÛLÉE",
+      description: "Caramelised vanilla crème with strawberry & black pepper sorbet",
+      price: 75
+    },
+    {
+      title: "SORBETS",
+      description: "Two scoops of: strawberry & black pepper, lemon or chocolate & lime",
+      price: 68
+    },
+    {
+      title: "TARTE AUX POMMES",
+      description: "Warm apple tart with tarte tatin ice cream",
+      price: 108
+    },
+    {
+      title: "FONDANT AUX CHOCOLATS",
+      description: "hot chocolate fondant with a melting middle served with chocolate & raspberry fondant ice cream",
+      price: 50
+    }
+  ],
+  main: [
+    {
+      title: "LAMB CASSOULET",
+      description: "Tender rump of lamb, served pink and on a rich cassoulet of haricot beans, bacon lardons, spinach, carrots, celery and onions (please allow for a slightly longer cooking time)",
+      price: 105
+    },
+    {
+      title: "STEAK BAGUETTE",
+      description: "Sliced British rump steak with red onion, watercress and Dijon mayonnaise, served with frites or house salad",
+      price: 99
+    },
+    {
+      title: "BOEUF BOURGUIGNON",
+      description: "Slow-cooked beef in a red wine sauce, with smoked bacon, mushrooms, roasted carrots, herb mash and crispy onions",
+      price: 120
+    },
+    {
+      title: "CROQUE MONSIEUR",
+      description: "Classic grilled Emmental cheese sandwich with smoked ham, served with frites or house salad",
+      price: 80
+    },
+    {
+      title: "SIDES",
+      description: "FRITES, MASH, NEW POTATOES, HOMEMADE DAUPHINOISE POTATOES, FRENCH BEANS, SPINACH",
+      price: 100
+    }
+  ],
+  drinks: {
+    wines: [
+      {
+        title: "LA CAMPAGNE, MERLOT",
+        description: "PAYS D'OC, FRANCE - warm and generous with rounded tannins and soft flavours of plum",
+        price: {
+          glass: 40,
+          bottle: 120
+        }
+      },
+      {
+        title: "GRANFORT, CABERNET SAUVIGNON",
+        description: "PAYS D'OC, FRANCE - blackcurrant flavours with a nose of red berries and sweet spice",
+        price: {
+          glass: 35,
+          bottle: 99
+        }
+      },
+      {
+        title: "LA CAMPAGNE, SAUVIGNON BLANC",
+        description: "PAYS D'OC FRANCE - aromas of herbs and gooseberries, with crisp zingy acidity on the palate",
+        price: {
+          glass: 40,
+          bottle: 120
+        }
+      }
+    ],
+    hotDrinks: [
+      {
+        title: "ESPRESSO",
+        description: "",
+        price: 14
+      },
+      {
+        title: "TEA",
+        description: "Earl Grey, Breakfast, Green, Camomile, Mint, Rooibos",
+        price: 15
+      },
+      {
+        title: "CAPPUCCINO",
+        description: "",
+        price: 22
+      }
+    ],
+    softDrinks: [
+      {
+        title: "MARSEILLE MULE",
+        description: "ginger beer, lime juice and bitters",
+        price: 42
+      },
+      {
+        title: "WINTER BERRY LEMONADE",
+        description: "lemon, blackberry, elderberry and raspberry",
+        price: 38
+      },
+      {
+        title: "BELU MINERAL WATER",
+        description: "still or sparkling",
+        price: 23
+      }
+    ]
+  }
+};
